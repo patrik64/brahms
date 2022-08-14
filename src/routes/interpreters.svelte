253b18be95d2c12112c437b34b3e3fd1b8e72877
Patrik Simic <patrik.simic@gmail.com>
2022-08-14 15:44:33 +0200

    interpreters

--- src/routes/interpreters.svelte
+++ src/routes/interpreters.svelte
@@ -1,6 +1,136 @@
 <div class="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
   <div class="max-w-7xl mx-auto text-gray-600 text-lg">
-    <div class="text-center text-4xl text-gray-600">Brahms-Interpret*innen</div>
-    <div class="pt-4">Ähnlich wie für Franz Schubert, dessen Lieder durch den Sänger Joseph Michael Vogl erst so richtig populär wurden, finden sich auch bei Brahms Künstler, die sich mehr als andere in den Dienst seiner Musik gestellt haben. Ohne Anspruch auf Vollständigkeit seien hier einige von ihnen in alphabetischer Reihenfolge näher vorgestellt.</div>
+    <div class="text-center text-4xl">Brahms-Interpret*innen</div>
+    <div class="py-4">Ähnlich wie für Franz Schubert, dessen Lieder durch den Sänger Joseph Michael Vogl erst so richtig populär wurden, finden sich auch bei Brahms Künstler, die sich mehr als andere in den Dienst seiner Musik gestellt haben. Ohne Anspruch auf Vollständigkeit seien hier einige von ihnen in alphabetischer Reihenfolge näher vorgestellt.</div>
+
+    <div class="pt-8 flex-1 flex items-center">
+      <div class="mx-auto w-auto relative z-0 inline-flex shadow-sm rounded-md">
+        <a href="#barbi" class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-teal-400 hover:bg-gray-50 focus:z-10 focus:outline-none">Alice Barbi</a>
+        <a href="#baumayer" class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-teal-400 hover:bg-gray-50 focus:z-10 focus:outline-none">Marie Baumayer</a>
+        <a href="#brull" class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-teal-400 hover:bg-gray-50 focus:z-10 focus:outline-none">Ignaz Brüll</a>
+      </div>
+    </div>
+    <div class="pt-1 pb-8 flex-1 flex items-center">
+      <div class="mx-auto w-auto relative z-0 inline-flex shadow-sm rounded-md">
+        <a href="#hellmesberger" class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-teal-400 hover:bg-gray-50 focus:z-10 focus:outline-none">Hellmesberger-Quartett</a>
+        <a href="#joachim" class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-teal-400 hover:bg-gray-50 focus:z-10 focus:outline-none">Joachim-Quartett</a>
+        <a href="#rose" class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-teal-400 hover:bg-gray-50 focus:z-10 focus:outline-none">Rosé-Quartett</a>
+      </div>
+    </div>
+
+    <!--barbi-->
+    <div id="barbi" class="py-4 text-center text-3xl">Alice Barbi (1858-1948)</div>
+    <div class="pt-4">Zunächst als Violinistin erfolgreich, begann Alice Barbi mit 20 Jahren eine Gesangsausbildung. Ihr Debüt als Sängerin gab sie am 2. April 1882, kurz darauf folgte ein Konzert in Rom vor der italienischen Königin unter der Leitung des Komponisten Giovanni Sgambati. Erfolgreiche Konzertreisen als königliche italienische Hofsängerin führten sie ab 1884 auch außerhalb Italiens, wobei sie in Wien erstmals 1889 auftrat. Hier etablierte sie sich rasch als Brahms-Sängerin par excellence.</div>
+    <div class="pt-4">Krönung ihrer Wiener Karriere, welche dank ihres Engagements im Rahmen von Wohltätigkeitskonzerten auch nach ihrer Vermählung mit Baron Wolff-Stomersee nie gänzlich zum Erliegen kam, war mit Sicherheit ihr Abschiedskonzert am 21. Dezember 1893, bei dem Brahms sie am Klavier begleitete. Alice Barbi engagierte sich aber auch nach Brahms‘ Tod für dessen Musik, wovon zahlreiche Wohltätigkeitskonzerte (meist reine „Brahms-Abende“) genauso zeugen wie die Tatsache, dass sie die Erste war, die ihren Konzerterlös für die Errichtung eines Brahms-Denkmals spendete. Dieses Denkmal, das heute im Resselpark steht, ist also nicht zuletzt auch eine Erinnerung an die „große“ Alice Barbi und ihre Verehrung für Johannes Brahms.</div>
+    <img class="m-auto border-solid border border-black mt-4" src="/images/iterpreters/barbi-alice.jpg" alt="barbi" />
+    <div class="text-center text-xs">Alice Barbi und Brahms vor dem Hotel Imperial, 1892 &copy; ÖNB</div>
+
+    <!--baumayer-->
+    <div id="baumayer" class="py-8 text-center text-3xl">Marie Baumayer (1851-1931)</div>
+    <div>
+      <div class="float-left p-2 flex-1 flex-col">
+        <img class="object-contain" src="/images/iterpreters/baumayer-marie.jpg" alt="baumayer" />
+        <div class="text-xs">&copy; Sophie Drinker Institut</div>
+      </div>
+      <p>Marie Baumayer kam 1871 nach Wien, wo sie am Konservatorium der Gesellschaft der Musikfreunde Klavier als Hauptfach in der Klasse von Julius Epstein studierte. Neben der Familie Wittgenstein war es Johannes Brahms, der sie förderte und auch freundschaftlich mit ihr verbunden war. Einer ihrer größten Erfolge war die Grazer Erstaufführung des Brahms’schen Klavierkonzerts Nr. 2 B-Dur, op. 83 am 5. November 1882. Dem Brahms-Biograph Max Kalbeck zufolge sei sie die erste gewesen, „die es wagen durfte, das B-dur-Konzert öffentlich zu spielen, ohne befürchten zu müssen, daß, wie bei einer minder glücklichen Rivalin, der gereizte Meister sich dafür mit dem zweideutigen Kompliment bedankte, er werde nun ein drittes, noch schwereres Klavierkonzert komponieren müssen, das von keinem Frauenzimmer gespielt werden könnte.“ (Kalbeck, Bd. IV, 21915, S. 31 f.).</p>
+      <p class="py-4">Ihrem Ruf als Brahms-Interpretin wurde Marie Baumayer auch nach Brahms' Tod gerecht, wobei ein Rezensent der Neuen Zeitschrift für Musik sogar von einem „Brahms-Cultus“ (NZfM 1900, S. 106) sprach, den die Künstlerin pflege. Zu ihren bevorzugten Kammermusikpartnern zählten denn auch - wenig überraschend - InterpretInnen, die ebenfalls dem Brahms-Kreis angehörten, darunter die Violinistin Marie Soldat-Roeger, deren Damenstreichquartett, der Cellist Robert Hausmann, der Klarinettist Richard Mühlfeld sowie das Hellmesberger- und das Rosé-Quartett.</p>
+    </div>
+    <br />
+    <br />
+
+    <!--brull-->
+    <div id="brull" class="py-8 text-center text-3xl">Ignaz Brüll (1846-1907)</div>
+    <div>
+      <div class="float-right p-2 flex-1 flex-col">
+        <img class="object-contain border-solid border border-black" src="/images/iterpreters/bruell-ignaz.jpg" alt="brull" />
+        <div class="text-xs">In seinem Heim Liechtensteinstr. 4, 9. Bezirk, 1892 &copy; ÖNB / Grillich</div>
+      </div>
+      <p>Als Zehnjähriger wurde Ignaz Brüll Schüler des berühmten Pianisten Julius Epstein, sein Debüt gab er mit 13 und bereits zwei Jahre später wurde sein 1. Klavierkonzert uraufgeführt. Ab da war Ignaz Brüll ein bekannter Name im Musikgeschehen seiner Zeit. 1872 begann er als Lehrer an der renommierten Wiener Klavierschule Horak, deren Mitdirektor er 1881 wurde. In den 1890er Jahren wandelte sich sein Repertoire; ab diesem Zeitpunkt trat er fast ausschließlich als Interpret neuer Werke von Carl Goldmark, Johannes Brahms (Uraufführung der Klavierstücke op. 76, op. 116, Nr. 1-3, op. 117, Nr. 1 und 2 sowie op. 119, Nr. 2) sowie eigener Kompositionen in Erscheinung.</p>
+      <p class="py-4">Doch nicht nur Brülls pianistische Qualitäten wurden geschätzt, sondern auch und vor allem seine menschlichen. So zählten etwa Carl Goldmark, Robert Fuchs, Eusebius Mandyczewski, Eduard Hanslick, Gustav Mahler, Theodor Billroth und nicht zuletzt Johannes Brahms zu seinem großen Freundeskreis. War er für Max Kalbeck der „Brahms-Pianist par Excellence“, so urteilte dieser gar über den liebevoll „Nazi“ titulierten Freund: „Wie etwas klingt, was ich gemacht habe, weiß ich erst, wenn ich's von Brüll gehört habe.“ (Quelle: <a rel="external" href="https://kulturstiftung.org/biographien/brull-ignaz-2" class="text-teal-400">Kulturstiftung der deutschen Vertriebenen</a>)</p>
+    </div>
+
+    <!--hellmesberger-->
+    <div id="hellmesberger" class="py-8 text-center text-3xl">Hellmesberger-Quartett</div>
+    <div>
+      <div class="float-left p-2 flex-1 flex-col">
+        <img class="object-contain border-solid border border-black" src="/images/iterpreters/hellmesberger-quartett-2.jpg" alt="hellmesberger2" />
+        <div class="text-xs">Im Jahr 1873: Joseph Hellmesberger senior (r. o.), Joseph Hellmesberger junior (r. u.),</div>
+        <div class="text-xs">Heinrich Röver (l. o.) und Sigismund Bachrich (l. u.) &copy; Wikipedia</div>
+      </div>
+      <p>1849 von Josef Hellmesberger senior gegründet, wurde dieses Streichquartett geradezu zum Synonym der Wiener Geigentradition in der Nachfolge Joseph Böhms und Ignaz Schuppanzighs. Einer der Gründe für diese Dominanz war mit Sicherheit der Tatsache geschuldet, dass das Hellmesberger-Quartett über lange Phasen hinweg das einzige regelmäßig konzertierende Ensemble in Wien war. Der Bedarf an Kammermusikdarbietungen war bis in die 1880er Jahre so gering, dass dieser neben dem fixen Hellmesberger-Zyklus problemlos durch gastierende Vereinigungen wie dem „Laub-Quartett“, Jean Beckers „Florentiner Quartett“ oder dem „Joachim-Quartett“ gedeckt werden konnte.</p>
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    
+    <div>
+      <div class="float-right p-2 flex-1 flex-col">
+        <img class="object-contain border-solid border border-black" src="/images/iterpreters/hellmesberger-quartett-1.jpg" alt="hellmesberger1" />
+        <div class="text-xs">Um 1898 &copy; A. Ehrlich, Das Streichquartett in Wort und BildA. H. Payne: Leipzig 1898, S. 17.</div>
+      </div>
+      <p>Eine Konstante der im Laufe der Jahre mehrmals wechselnden Besetzung war die Beteiligung eines oder mehrerer Mitglieder der Familie Hellmesberger, wobei nach dem Ausscheiden des Gründers dessen Sohn Josef Hellmesberger junior die Rolle des Primarius übernahm.</p>
+      <p class="py-4">Die Programme waren größtenteils so aufgebaut, dass zwischen zwei Streichquartette ein anderweitig besetztes Werk (Klaviertrio, Streichsextett, Violinsonate etc.) eingefügt wurde. Dafür wurden so namhafte KünstlerInnen gewonnen wie Julius Epstein, Alfred Grünfeld, Moriz Rosenthal, Anton Rubinstein oder Clara Schumann. Johannes Brahms feierte gar sein Debüt in Wien am 16. November 1862 im Rahmen eines Kammermusikabends des Hellmesberger-Quartetts mit der Aufführung seines Klavierquartetts g-Moll, op. 25.</p>      
+      <p class="py-4">War mit dem Ausscheiden des Ensemblegründers eine deutliche Qualitätseinbuße zu verzeichnen, wurde die Konzerttätigkeit mit der Ernennung Josef Hellmesbergers jun. im Jahr 1901 zum Hofkapellmeister nicht mehr fortgesetzt. Das seit 1883 bestehende Rosé-Quartett übernahm damit endgültig die Position der alleinigen Quartett-Größe und Hüterin der Kammermusiktradition in Wien.</p>      
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <!--joachim-->
+    <div id="joachim" class="py-4 text-center text-3xl">Joachim-Quartett</div>
+    <img class="m-auto border-solid border border-black mt-4" src="/images/iterpreters/joachim-quartett.jpg" alt="joachim" />
+    <div class="text-center text-xs">1884: Heinrich de Ahna (l.), Robert Hausmann (o.), Emanuel Wirth (r.), Joseph Joachim (u.) &copy; Wikipedia</div>
+    <div class="pt-4">1869 von dem Violinvirtuosen und Brahms-Freund Joseph Joachim gegründet, wurde diese Quartettformation, die bis zu Joachims Tod 1907 bestand, zu einem der Hauptrepräsentanten deutscher Musikkultur im späten 19. und frühen 20. Jahrhundert. Die Besetzung erlebte zwar einige Wechsel, die Qualität blieb aber dank Joachims Führung stets auf unumstritten hohem Niveau.</div>
+    <ul class="pl-8" style="list-style-type: circle;">
+      <li class="pt-2 pl-2">1. Violine: Joseph Joachim</li>
+      <li class="pt-2 pl-2">2. Violine: Ernst Schiever (1869-1872), Heinrich de Ahna (1872-1892), Johann Kruse (1892-1897), Carl Halir (1897-1907)</li>
+      <li class="pt-2 pl-2">Viola: Heinrich de Ahna (1869-1872), Eduard Rappoldi (1872-1877), Emanuel Wirth (1877-1906), Karl Klingler (1906-1907)</li>
+      <li class="pt-2 pl-2">Violoncello: Wilhelm Müller (1869-1879), Robert Hausmann (1879-1907)</li>
+    </ul>
+    <div class="pt-4">Auf Programmen des Joachim-Quartetts fehlte ein Komponist selten: Johannes Brahms. Joachim trat von Beginn an für die Werke seines Freundes ein und durfte u.a. die Streichquartette op. 51, Nr. 2 und op. 67 zur Uraufführung bringen.</div>
+
+    <!--rose-->
+    <div id="rose" class="py-8 text-center text-3xl">Rosé-Quartett</div>
+    <div>
+      <div class="float-right p-2 flex-1 flex-col">
+        <img class="border-solid border border-black object-contain" src="/images/iterpreters/rose-quartett.jpg" alt="rose" />
+        <div class="text-xs">Um 1884 &copy; ÖNB</div>
+      </div>
+      <p>„nur für das Rosé-Quartett und auf Wunsch von Herrn Hummer“, so heißt es von Brahms in einer handschriftlichen Anmerkung am Rand des Blatts der 1. Violine seines Streichquintett op. 111 (Quelle: Styra Avins / Josef Eisinger: Sechs unveröffentlichte Briefe von Brahms. Aus: Brahms-Studien Band 13, 2002). Damit erlaubte er den Streichern bei der Uraufführung seines Streichquintetts am 11. November 1890 im Salon Bösendorfer, die Anfangstakte piano statt dem geforderten forte zu spielen, um die Stimme des Cellisten Reinhold Hummer besser hervortreten zu lassen - ein Zugeständnis, das von Brahms‘ hoher Wertschätzung für das Rosé-Quartett zeugt. </p>
+      <p class="py-4">1882 von Arnold Rosé (geborener Rosenblum) und seinem Bruder Eduard in Wien gegründet, konzertierte dieses Ensemble erstmals am 22. Jänner 1883, das letzte Konzert fand 1945 in der Londoner Emigration statt. Die Besetzung wechselte im Lauf der Zeit etliche Male: </p>
+      <ul class="pl-8" style="list-style-type: circle;">
+        <li class="pt-2">1. Violine: Arnold Rosé (1882-1945)</li>
+        <li class="pt-2">2. Violine: Julius Egghard d. J. (1882-1883), Anton Loh (1884-1889), August Siebert (1890-1896), Albert Bachrich (1897-1904), Paul Fischer (1905-1938)</li>
+        <li class="pt-2">Viola: Anton Loh (1882-1883), Sigismund Bachrich (1884-1894), Hugo von Steiner (1895-1901), Anton Ruzitska (1901-1929), Max Handl (1930-1933), Julius Stwertka (1934-1938)</li>
+        <li class="pt-2">Violoncello: Eduard Rosé (1882-1883), Reinhold Hummer (1884-1900), Friedrich Buxbaum (1901-1920 und 1922-1945), Anton Walter (1921)</li>
+      </ul>
+
+      <p class="py-4">Beginnend mit der Saison 1882/1883 waren Brahms' Werke auf den Programmen des Rosé-Quartetts stets gut vertreten und Brahms selbst brachte gemeinsam mit dem Klarinettisten Richard Mühlfeld seine Spätwerke, die beiden Klarinettensonaten op. 120, im Rahmen von Kammermusikabenden des Rosé-Quartetts zur Uraufführung (Nr. 1: 11. Januar 1895, Nr. 2: 8. Januar 1895).</p>
+    </div>
+    <br />
+    <br />
   </div>
 </div>
