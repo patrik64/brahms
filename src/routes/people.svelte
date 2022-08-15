9b3b2dd9c25b610141c53cc4257d976d18b61ab2
Patrik Simic <patrik.simic@gmail.com>
2022-08-15 21:07:32 +0200

    personen

--- src/routes/people.svelte
+++ src/routes/people.svelte
@@ -11,6 +11,29 @@ import people from '$lib/data/people.json';
       <h1 class="text-3xl text-gray-600 text-center">Personen</h1>
     </div>
   </div>
+  <ul class="py-12 mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
+    {#each people as person }
+    <li>
+      <a href="{`/person/${person.id}`}" class="space-y-4">
+        {#if person.image && person.image.length >0}
+          <img class="mx-auto h-20 w-20 rounded-full lg:w-24 lg:h-24 object-cover" src={`/images/people/thumbs/${person.image}`} alt="{person.surname}" />
+        {:else}
+        <svg class="mx-auto h-20 w-20 rounded-full lg:w-24 lg:h-24 object-cover text-gray-300 border border-gray-100" fill="currentColor" viewBox="0 0 24 24">
+          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+        {/if}
+        <div class="space-y-2">
+          <div class="text-xs font-medium lg:text-sm text-center">
+            <h3>
+              <div class="text-teal-400">{person.name} {person.surname}</div>
+            </h3>
+          </div>
+        </div>
+      </a>
+    </li>
+    {/each}
+  </ul>
+
   <div class="-mx-4 mt-10 ring-1 ring-gray-300 sm:-mx-6 md:mx-0 md:rounded-lg">
     <table class="min-w-full divide-y divide-gray-300">
       <thead>
@@ -39,4 +62,5 @@ import people from '$lib/data/people.json';
       </tbody>
     </table>
   </div>
+  
 </div>
