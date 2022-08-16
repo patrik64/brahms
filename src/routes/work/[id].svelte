0fb2f765c34ca6f3450f985d94f97f762a1a9f02
Patrik Simic <patrik.simic@gmail.com>
2022-08-16 03:39:17 +0200

    works

---
+++ src/routes/work/[id].svelte
@@ -0,0 +1,30 @@
+<script context="module">
+    export const load = ({ params }) => {
+        const id = params.id;
+
+        return {
+            props: {
+                id
+            }
+        };
+    }
+</script>
+
+<script>
+    export let id;
+    import works from '$lib/data/works.json';
+
+    let work = works[id-1];
+</script>
+
+<div class="center-screen">work in progress</div>
+
+<style>
+    .center-screen {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        min-height: 70vh;
+    }
+</style>
